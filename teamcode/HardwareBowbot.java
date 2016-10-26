7b365d932dcea0ec36778466d36ba86278a56650
Bowbotics <bowboticsteam@gmail.com>
2016-10-26 17:11:17 -0500

    Add files via upload
    Work done on 10/26

--- teamcode/HardwareBowbot.java
+++ teamcode/HardwareBowbot.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * This is NOT an opmode.
  *
  * This class can be used to define all the specific hardware for a single robot.
- * In this case that robot is a Pushbot.
+ * In this case that robot is a Bowbot.
  * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
  *
  * This hardware class assumes the following device names have been configured on the robot:
@@ -24,11 +24,11 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class HardwareBowbot
 {
     /* Public OpMode members. */
-    public DcMotor  leftMotor   = null;
-    public DcMotor  rightMotor  = null;
-    public DcMotor  armMotor    = null;
-    public Servo    leftClaw    = null;
-    public Servo    rightClaw   = null;
+    public DcMotor  Left_Drive   = null;
+    public DcMotor  Right_Drive  = null;
+    public DcMotor  Sweeper_Motor    = null;
+  //  public Servo    buttonpusher    = null;//
+
 
     public static final double MID_SERVO       =  0.5 ;
     public static final double ARM_UP_POWER    =  0.45 ;
@@ -49,28 +49,28 @@ public class HardwareBowbot
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        leftMotor   = hwMap.dcMotor.get("left_drive");
-        rightMotor  = hwMap.dcMotor.get("right_drive");
-        armMotor    = hwMap.dcMotor.get("left_arm");
-        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        Left_Drive   = hwMap.dcMotor.get("Left_Drive");
+        Right_Drive  = hwMap.dcMotor.get("Right_Drive");
+        Sweeper_Motor    = hwMap.dcMotor.get("Sweeper_Motor");
+        Left_Drive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        Right_Drive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
         // Set all motors to zero power
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
-        armMotor.setPower(0);
+        Left_Drive.setPower(0);
+        Right_Drive.setPower(0);
+        Sweeper_Motor.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
-        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Left_Drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Right_Drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Sweeper_Motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Define and initialize ALL installed servos.
-        leftClaw = hwMap.servo.get("left_hand");
-        rightClaw = hwMap.servo.get("right_hand");
-        leftClaw.setPosition(MID_SERVO);
-        rightClaw.setPosition(MID_SERVO);
+       // buttonpusher = hwMap.servo.get(buttonpusher);//
+        //rightClaw = hwMap.servo.get("right_hand");//
+      //  leftClaw.setPosition(MID_SERVO);//
+        //rightClaw.setPosition(MID_SERVO);//
     }
 
     /***
@@ -81,6 +81,8 @@ public class HardwareBowbot
      *
      * @param periodMs  Length of wait cycle in mSec.
      * @throws InterruptedException
+     * ???????
+     *  profit
      */
     public void waitForTick(long periodMs) throws InterruptedException {
 
@@ -90,7 +92,7 @@ public class HardwareBowbot
         if (remaining > 0)
             Thread.sleep(remaining);
 
-        // Reset the cycle clock for the next pass.
+        // Reset the cycle clock for the next pass. O.O
         period.reset();
     }
 }
